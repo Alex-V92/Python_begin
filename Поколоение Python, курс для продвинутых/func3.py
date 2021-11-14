9e06e9d8a6c4a2b7167d3eec3caddd2d05432bb2
AlexejVladimirov <alexej.92@yandex.ru>
2021-11-14 14:21:46 +0300

    Функция сортировки, использующая filter, map и reduce

--- Поколоение Python, курс для продвинутых/func3.py
+++ Поколоение Python, курс для продвинутых/func3.py
@@ -31,3 +31,28 @@ numbers = filter(double_without7,numbers)
 print(numbers)
 print(sum(map(lambda x:x**2,numbers)))'''
 
+from functools import reduce
+
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
+new_list = list(filter(lambda x:x[2] == 'primary' and x[1] > 10000000, data))
+new_list = list(map(lambda x: x[0], new_list))
+print('Cities:',reduce(lambda x,y: x +', '+ y,sorted(new_list)))
