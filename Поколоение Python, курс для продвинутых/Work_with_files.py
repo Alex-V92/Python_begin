d0fedcb30ad3d04bc182e2708332f9574e3cd56c
AlexejVladimirov <alexej.92@yandex.ru>
2021-11-17 18:30:31 +0300

    Последнии задания курса

--- Поколоение Python, курс для продвинутых/Work_with_files.py
+++ Поколоение Python, курс для продвинутых/Work_with_files.py
@@ -54,5 +54,52 @@ with open('first_names.txt') as first_file, open('last_names.txt') as last_file:
         print(list_values)
         return [(dict(zip(list_dict,i))) for i in list_values]
 print(read_csv())'''
+'''from random import randint
+test_list = [randint(111,777) for i in range(25)]
+with open('random1.txt','w') as output:
+    print(*test_list,  sep = '\n', file=output)'''
 
 
+
+'''with open('input.txt','r') as input_file, open('output.txt','w') as output_file:
+    for i in enumerate(input_file.readlines(),1):
+        print(str(i[0]) +')', i[1].strip('\n'), file=output_file)'''
+
+'''with open('new_scores.txt','w', encoding='utf-8') as output_file, open('class_scores.txt','r',encoding='utf-8') as input_file :
+    for line in input_file.readlines():
+        asnwer_list = list(map(lambda x: x if x.isalpha() else int(x) +5 if int(x) < 95 else 100, line.split() ))
+        print(*asnwer_list, file=output_file)'''
+
+'''with open('goats.txt','r') as file_goats, open('answer.txt','w') as file_answer:
+    answer_dict = {}
+    total_goat = 0
+    for line in file_goats.readlines()[1:]:
+        if line.strip('\n') == 'GOATS' or line.strip('\n') == 'COLOURS':
+            continue
+        if line.strip('\n') in answer_dict:
+            answer_dict[line.strip('\n')] += 1
+            total_goat += 1
+        else:
+            answer_dict[line.strip('\n')] = 0
+    for key,value in answer_dict.items():
+        if total_goat / 100 * value > 7:
+            print(key, file=file_answer)'''
+
+'''n = int(input())
+list_files = [input() for i in range(n)]
+with open('output.txt','r') as file_output:
+    for line in list_files:
+        with open(line) as file_input:
+            file_output.write(file_input)'''
+'''with open('logfile.txt','r',encoding='utf-8') as file_log, open('output.txt','w',encoding='utf-8') as file_answer:
+    dict_answer = {}
+    for line in file_log.readlines():
+        time_user = list(line.strip('\n').split(', '))
+        if time_user[0] in dict_answer:
+            dict_answer[time_user[0]] += (int(time_user[2][:2]) * 60 +int(time_user[2][3:]) ) - (int(time_user[1][:2]) * 60 +int(time_user[1][3:]) )
+        else:
+            dict_answer[time_user[0]] = (int(time_user[2][:2]) * 60 + int(time_user[2][3:])) - (
+                        int(time_user[1][:2]) * 60 + int(time_user[1][3:]))
+    for key, value in dict_answer.items():
+        if value >= 60:
+            print(key, file=file_answer)'''
