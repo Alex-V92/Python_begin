6bd8325a5f2005dba1e61789f512f5ef5af6a77d
AlexejVladimirov <alexej.92@yandex.ru>
2021-11-17 23:43:43 +0300

    Последний экзамен на курсе. 100% закрыто

--- Поколоение Python, курс для продвинутых/Work_with_files.py
+++ Поколоение Python, курс для продвинутых/Work_with_files.py
@@ -103,3 +103,74 @@ with open('output.txt','r') as file_output:
     for key, value in dict_answer.items():
         if value >= 60:
             print(key, file=file_answer)'''
+
+
+
+
+'''with open('grades.txt') as file_open:
+    total = 0
+    for line in file_open.readlines():
+        student = line.split()
+        if len(list(filter(lambda x:int(x) >= 65, student[1:]))) == 3:
+            total += 1
+    print(total)'''
+
+'''with open('words.txt') as file_open:
+    list_t = list(file_open.read().split())
+    print(*list(filter(lambda x:len(x) == len(max(list_t, key=len)) ,list_t)),sep='\n')'''
+
+'''#name_file = input()
+with open('input.txt') as file_open:
+    print(*file_open.read().split('\n')[-10:],sep='\n')'''
+'''d = { 'а': 'a', 'к': 'k', 'х': 'h', 'б': 'b', 'л': 'l', 'ц': 'c', 'в': 'v', 'м': 'm', 'ч': 'ch', 'г': 'g', 'н': 'n', 'ш': 'sh', 'д': 'd', 'о': 'o', 'щ': 'shh', 'е': 'e', 'п': 'p', 'ъ': '*', 'ё': 'jo', 'р': 'r', 'ы': 'y', 'ж': 'zh', 'с': 's', 'ь': "'", 'з': 'z', 'т': 't', 'э': 'je', 'и': 'i', 'у': 'u', 'ю': 'ju', 'й': 'j', 'ф': 'f', 'я': 'ya', 'А': 'A', 'К': 'K', 'Х': 'H', 'Б': 'B', 'Л': 'L', 'Ц': 'C', 'В': 'V', 'М': 'M', 'Ч': 'Ch', 'Г': 'G', 'Н': 'N', 'Ш': 'Sh', 'Д': 'D', 'О': 'O', 'Щ': 'Shh', 'Е': 'E', 'П': 'P', 'Ъ': '*', 'Ё': 'Jo', 'Р': 'R', 'Ы': 'Y', 'Ж': 'Zh', 'С': 'S', 'Ь': "'", 'З': 'Z', 'Т': 'T', 'Э': 'Je', 'И': 'I', 'У': 'U', 'Ю': 'Ju', 'Й': 'J', 'Ф': 'F', 'Я': 'Ya' }
+with open('cyrillic.txt',encoding='utf-8') as file_open, open('transliteration.txt','w') as file_save:
+    for letter in file_open.read():
+        if letter in d:
+            print(d[letter],file=file_save,end='')
+        else:
+            print(letter,file=file_save,end='')'''
+
+'''with open('input.txt',encoding='utf-8') as file_open:
+    list_test = list([i for i in file_open.readlines()])
+
+    total_desc = 0
+    list_funcs = []
+    for i in range(len(list_test)):
+        word = ''
+        if list_test[i][:3] == 'def':
+            if i == 0:
+                for j in list_test[i][4:]:
+                    if j == '(':
+                        list_funcs.append(word)
+                        break
+                    else:
+                        word += j
+                continue
+            elif list_test[i-1][0] != '#':
+                for j in list_test[i][4:]:
+                    if j == '(':
+                        list_funcs.append(word)
+                        break
+                    else:
+                        word += j
+            else:
+                total_desc += 1
+                continue
+    if len(list_funcs) == 0:
+        print('Best Programming Team')
+    else:
+        print(*list_funcs,sep='\n')'''
+
+'''with open('forbidden_words.txt') as file_regect, open('data (1).txt',encoding='utf-8') as file_output:
+    stop_words = file_regect.read().lower().split()
+    answer = file_output.read()
+    answer_low = answer.lower()
+    for word in stop_words:
+        answer_low = answer_low.replace(word,'*'*len(word))
+    for i in range(len(answer)):
+        if answer[i].lower() == answer_low[i]:
+            print(answer[i],end='')
+        else:
+            print('*',end='')'''
+
+
